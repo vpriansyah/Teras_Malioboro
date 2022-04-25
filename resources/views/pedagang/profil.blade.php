426b96e2d9c267d31cc4b672f67e5d24df17bfb9
vpriansyah <vpriansyah@gmail.com>
2022-04-26 01:36:04 +0700

    update profile pedagang

--- resources/views/pedagang/profil.blade.php
+++ resources/views/pedagang/profil.blade.php
@@ -52,11 +52,49 @@
             </span>
         </div>
     </section>
-    <div class="container">
-    <h2>Profile Pedagang</h2>
-    <br>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni culpa et, sed doloremque eius molestias, recusandae iure illum optio quas ea veritatis odit ad hic error dolore deleniti nostrum itaque.</p>
-    </div>
+    <section class="iq-feature1 overview-block-ptb grey-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-md-6 col-sm-12 iq-mtb-15">
+                    <div class="iq-blog text-left iq-ptb-30 iq-pr-30 iq-pl-30">
+
+                            <div>
+                                <h5 class="mx-auto"><b>Profil Toko</b></h5>
+                            </div>
+                            <br>
+                            <div class="col-lg-6 align-self-center">
+                            <img class="iq-works-img" style="width: 370px; height: 250px; border-radius:10%; align-self-center;" src="{!! asset('images/fototoko.png') !!}" alt="">
+                            </div>
+                            <br>
+                            <br>
+                            <p>Nama Toko : {{ $nama }}</p>
+                            <p>Jam Opersioanal :</p>
+                            <p>Dagangan :</p>
+                            <p>Lokasi :</p>
+                      
+                    </div>
+                </div>
+                <div class="col-lg-7 col-md-6 col-sm-12 iq-mtb-15">
+                    <div class="iq-blog text-left iq-ptb-30 iq-pr-30 iq-pl-30">
+
+                            <div>
+                                <h5 class="mx-auto"><b>Profil Pedagang</b></h5>
+                            </div>
+                            <br>
+                        <div class="content-blog">
+                            <p>Nama Lengkap :</p>
+                            <p>Nomor HP :</p>
+                            <p>Gender :</p>
+                            <p>Alamat :</p>
+                            <p>Paguyuban :</p>
+                            <p>NIK :</p>
+                            <p>No KK :</p>
+                            <p>Nomor NIB :</p>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        </section>
     </div>
     
     </body>
