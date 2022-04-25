362294527a587ba2964a89ff15216b220b6a822b
MuadzVarokah <muadzyo@gmail.com>
2022-04-26 00:14:14 +0700

    Revert "Update web.php"
    This reverts commit eee28061ec444c736f7e362eca2f9fde7f36e87a.

--- routes/web.php
+++ routes/web.php
@@ -48,17 +48,13 @@ Route::get('pedagang/informasi', function () {
 });
 
 //Publik
-Route::get('/', [PublikController::class,'index']);
+Route::get('/', function () {
+    return view('publik/index');
+});
 
-Route::get('/publik/', [PublikController::class,'index']);
-
-// Route::get('/', function () {
-//     return view('publik/index');
-// });
-
-// Route::get('/publik/', function () {
-//     return view('publik/index');
-// });
+Route::get('/publik', function () {
+    return view('publik/index');
+});
 
 Route::get('/publik/selfassessment', function () {
     return view('/publik/selfassessment');
