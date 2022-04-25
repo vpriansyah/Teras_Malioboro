32a286edae8e9a7fbe03f5018a66f2af79916208
Dicky <dickylumbantobing@student.uns.ac.id>
2022-04-25 09:39:18 +0700

    Update agenda.blade.php

--- resources/views/publik/agenda.blade.php
+++ resources/views/publik/agenda.blade.php
@@ -13,21 +13,33 @@
     </div>
     <div class="container">
       <div class="row">
-      <div>
-      <center><h4>Halaman Agenda untuk menampilkan list kegiatan Teras Malioboro</h4></center>
-      <table class="table">
-        <table class="row">
-        <tr>
-            <th>No</th>
-            <th>Nama</th>
-            <th>Email</th>
-            <th>Alamat</th>
-          </tr>
-        </thead>
-        </table>
-      </div>
-      </div>
+        <div class="col-lg-12 col-md-12">
+          <div class="heading-title text-center">
+            <p><h4>Halaman Agenda untuk menampilkan list kegiatan Teras Malioboro</h4></p>
+          </div>
+        </div>
       </div>
     </div>
+    <div class="container table-responsive">
+    <table class="table table-bordered table-striped table-hover">
+    <caption>Agenda Teras Malioboro</caption>
+			<thead>
+				<tr>
+					<th>Waktu</th>
+					<th>Nama</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>23 Agustus 2022</td>
+					<td>Pukul orang gabut</td>	
+				</tr>
+				<tr>
+					<td>25 Oktober 2022</td>
+					<td>Makan-Makan Di wedangan</td>
+			</tbody>
+		</table>
+ 
+	</div>
 </section>
 @endsection
