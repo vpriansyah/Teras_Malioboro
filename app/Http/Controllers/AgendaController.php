02ff9bd1b7372989c3af8a6ac3e46f0679ce5454
Dicky <dickylumbantobing@student.uns.ac.id>
2022-04-26 22:58:07 +0700

    Update AgendaController.php

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+<<<<<<< Updated upstream
 
+=======
+use Illuminate\Database\Eloquent\Model;
+>>>>>>> Stashed changes
  
 class AgendaController extends Controller
 {
