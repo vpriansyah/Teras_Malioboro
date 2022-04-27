3a87859202d2b5c2e455c9aa1fec61ef84cbc76c
Indra7667 <indra.kusuma.a1.223@gmail.com>
2022-04-27 10:12:07 +0700

    Publik galeri null image

--- resources/views/publik/galeri.blade.php
+++ resources/views/publik/galeri.blade.php
@@ -10,13 +10,15 @@
           </div>
         </div>
       </div>
+      <!--navbar-->
       <div class="tab-v1">
         <ul class="nav nav-tabs">
-            @foreach($kat_brg as $kat)
-              <li class="nav-item">
-                <a class="nav-link" href="{{$kat->nama}}" data-toggle="tab">{{$kat->nama}}</a></li>
-            @endforeach
-            
+          <li class="nav-item">
+            <a class="nav-link" href="#" data-toggle="tab">Semua</a></li>
+          @foreach($kat_brg as $kat)
+            <li class="nav-item">
+              <a class="nav-link" href="{{$kat->nama}}" data-toggle="tab">{{$kat->nama}}</a></li>
+          @endforeach
           <li>
           <form class="d-flex">
                 <div class="input-group">                    
@@ -27,27 +29,34 @@
           </li>
         </ul>
       </div>
-
+      <!---->
+      
+      <!--card galeri-->
       @foreach($data_pkl as $data)
       <div class="row justify-content-md-center">
         <div class="col-lg-3 col-md-6 col-sm-6 iq-mtb-15">
           <a href="{{$data->id}}">
             <div class="iq-blog text-left iq-ptb-30 iq-pr-30 iq-pl-30">
               <div>
-                <!--<img class="mx-auto d-block" src="{ asset('images/' . $data->foto_lapak . '') }" style="width: 100%;">-->
-                <img src="{!! asset('images/Publik_Galeri/' . $data->foto_lapak . '') !!}" class="d-block w-100" alt="''.$data->foto_lapak.''">
+                @php 
+                $foto = ($data->foto_lapak);
+                if ($data->foto_lapak == null) $foto= "notfound.jpg";
+                
+                
+                @endphp
+                <img src="{!! asset('images/Publik_Galeri/' . $foto . '') !!}" class="d-block w-100" alt="''.$data->foto_lapak.''">
                 <center> <h5 class="iq-tw-6 iq-pb-10">{{$data->nama_lengkap}}</h5> </center>
                 <center>{{$data->dagangan}}</center>
-                
-
               </div>
             </div>
           </a>
         </div>
       </div>
       @endforeach
+      <!---->
     </div>
   </section>
+
   {{-- Modal Data Pedagang --}}
     <div class="modal fade" id="data_pedagang" tabindex="-1" role="log">
         <div class="modal-dialog modal-lg" role="document">
@@ -79,5 +88,5 @@
             </div>
         </div>
     </div>
-    {{-- Modal Data Pedagang END --}}
+  {{-- Modal Data Pedagang END --}}
 @endsection
