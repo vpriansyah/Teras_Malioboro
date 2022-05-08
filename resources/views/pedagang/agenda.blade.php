65dbdc61505f978c700a557c057c57336d46c81c
arjunsetyawan <harjunosetyawan@gmail.com>
2022-05-08 23:13:36 +0700

    halaman agenda sementara

--- resources/views/pedagang/agenda.blade.php
+++ resources/views/pedagang/agenda.blade.php
@@ -1,5 +1,117 @@
 @extends('pedagang.layouts.footer')
 @include('pedagang.partials.navbar')
 @section('content')
-    <h1>HALAMAN AGENDA</h1>
+    <div class="container">
+        <section class style="margin-top: 30px">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="postgrid">
+                                <figure><img src="{!! asset('images/tm1.png') !!}" width="360" height="240"></figure>
+                                <h2 class="title">CONTOH AGENDA</h2>
+                                <p class="excerp">Acara Open Recruitment Tema – Hari dan Tanggal Sabtu, 25 September
+                                    2021</p>
+                                <a class=" btn btn-success" href="" target>Selengkapnya</a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="postgrid">
+                                <figure><img src="{!! asset('images/tm1.png') !!}" width="360" height="240"></figure>
+                                <h2 class="title">CONTOH AGENDA</h2>
+                                <p class="excerp">Acara Open Recruitment Tema – Hari dan Tanggal Sabtu, 25 September
+                                    2021</p>
+                                <a class=" btn btn-success" href="" target>Selengkapnya</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="postgrid">
+                                <figure><img src="{!! asset('images/tm1.png') !!}" width="360" height="240"></figure>
+                                <h2 class="title">CONTOH AGENDA</h2>
+                                <p class="excerp">Acara Open Recruitment Tema – Hari dan Tanggal Sabtu, 25
+                                    September
+                                    2021</p>
+                                <a class=" btn btn-success" href="" target>Selengkapnya</a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="postgrid">
+                                <figure><img src="{!! asset('images/tm1.png') !!}" width="360" height="240"></figure>
+                                <h2 class="title">CONTOH AGENDA</h2>
+                                <p class="excerp">Acara Open Recruitment Tema – Hari dan Tanggal Sabtu, 25
+                                    September
+                                    2021</p>
+                                <a class=" btn btn-success" href="" target>Selengkapnya</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="postgrid">
+                                <figure><img src="{!! asset('images/tm1.png') !!}" width="360" height="240"></figure>
+                                <h2 class="title">CONTOH AGENDA</h2>
+                                <p class="excerp">Acara Open Recruitment Tema – Hari dan Tanggal Sabtu, 25
+                                    September
+                                    2021</p>
+                                <a class=" btn btn-success" href="" target>Selengkapnya</a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="postgrid">
+                                <figure><img src="{!! asset('images/tm1.png') !!}" width="360" height="240"></figure>
+                                <h2 class="title">CONTOH AGENDA</h2>
+                                <p class="excerp">Acara Open Recruitment Tema – Hari dan Tanggal Sabtu, 25
+                                    September
+                                    2021</p>
+                                <a class=" btn btn-success" href="" target>Selengkapnya</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="postgrid">
+                                <figure><img src="{!! asset('images/tm1.png') !!}" width="360" height="240"></figure>
+                                <h2 class="title">CONTOH AGENDA</h2>
+                                <p class="excerp">Acara Open Recruitment Tema – Hari dan Tanggal Sabtu,
+                                    25 September
+                                    2021</p>
+                                <a class=" btn btn-success" href="" target>Selengkapnya</a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="postgrid">
+                                <figure><img src="{!! asset('images/tm1.png') !!}" width="360" height="240"></figure>
+                                <h2 class="title">CONTOH AGENDA</h2>
+                                <p class="excerp">Acara Open Recruitment Tema – Hari dan Tanggal Sabtu,
+                                    25 September
+                                    2021</p>
+                                <a class=" btn btn-success" href="" target>Selengkapnya</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box-news" style="padding-bottom: 20px">
+                        <h3 class="titlehead">Berita Terbaru</h3>
+                        <br>
+                        <ul>
+                            <li>
+                                <h5 class="title"><a href="">Teras Maliboro dinilai Efektif</a></h5>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                                <div class="row">
+                                    <div class="col-xs-6 left"> </div>
+                                    <div class="col-xs-6 text-right">
+                                        <a class="read-more" href=""><span class="fa fa-angle-double-right"></span></a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
 @endsection
