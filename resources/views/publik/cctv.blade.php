8e462f3481a02370f00375b7ec5def32a2d60ff0
FathulNisaAini <92559540+FathulNisaAini@users.noreply.github.com>
2022-05-10 09:38:44 +0700

    Update cctv.blade.php

--- resources/views/publik/cctv.blade.php
+++ resources/views/publik/cctv.blade.php
@@ -8,6 +8,34 @@
           <div class="heading-title-layanan text-center">
             <h2 class="title iq-tw-6">CCTV Teras</h2>
           </div>
+          <center>
+          <h3>Kepadatan Pengunjung Teras Malioboro</h3>
+          <div name="description">Jumlah pengunjung di Teras Malioboro 2, Kota Jogja, mengalami peningkatan yang signifikan saat libur Lebaran 2022.</div>
+          <p></p>
+          <div class="myBox">Sumber : <a href="https://www.solopos.com/membeludak-pengunjung-teras-malioboro-capai-25-000-orang-dalam-sehari-1311018">SOLOPOS</a> </div>
+          <p></p>
+          </center>
+          <center>
+          <div class="carousel-item active">
+            <img class="d-block w-60" src="{!! asset('images/kepadatan.jpg') !!}" alt="First slide">
+          </div>
+            </center>
+          <p></p>
+          <p>
+            <strong>Solopos.com, JOGJA</strong>
+            <em>
+              <strong>-</strong>
+            </em>
+            Pengunjung
+            <a href="https://www.solopos.com/tag/teras-malioboro">Teras Malioboro</a>
+            2 mengalami peningkatan yang signifikat pada momen libur Lebaran 2022. Bahkan, dalam satu hari, pada Rabu (4/5/2022), pengunjung Teras Malioboro 2022 mencapai 25.000 orang.
+          </p>
+          <p>Kepala Unit Pelaksana Teknis Pengelolaan Kawasan Cagar Budaya (UPT PKCB) Jogja, Ekwanto, mengatakan jumlah pengunjung masih berpotensi bertambah di Teras Malioboro 2. “Puncak kunjungan di Teras Malioboro 2 diprediksi tanggal 6, 7, dan 8 Mei 2022. Jumlah pengunjung bisa mencapai lebih dari 30.000 orang,” kata Ekwanto, Kamis (5/5/2022).</p>
+          <p>
+            <em>
+              <strong>Baca juga:&nbsp;<a href="https://www.solopos.com/ramai-pkl-pengunjung-teras-malioboro-2-malah-mengeluh-kenapa-1310450">Ramai! PKL &amp; Pengunjung Teras Malioboro 2</a></strong>
+            </em>
+          </p>                   
         </div>
       </div>
     </div>
