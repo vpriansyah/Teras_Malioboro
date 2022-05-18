490ee93935906fa205d78677e3a2d78e86e4194c
Dicky <dickylumbantobing@student.uns.ac.id>
2022-05-18 21:37:42 +0700

    agenda update

--- resources/views/publik/agenda.blade.php
+++ resources/views/publik/agenda.blade.php
@@ -13,7 +13,7 @@
                 </div>
             </div>
         </div>
-        <div class="container table-responsive">
+        <!--<div class="container table-responsive">
             <table class="table table-bordered table-striped table-hover">
                 <caption>Agenda Teras Malioboro</caption>
 
@@ -28,7 +28,7 @@
                     @php $no++ @endphp
                     <tr>
                         <td>{{ $no }}</td>
-                        <td>{{ $item->waktu }}</td>
+                        <td>{{ $item->tanggal }}</td>
                         <td>{{ $item->nama }}</td>
                     </tr>
                 @endforeach
@@ -38,7 +38,78 @@
 
         </div>
         <div class="container">
-        {{ $info_agenda->links() }}
-        </div>
+        
+        </div>-->
+        <!-------------------- sdsa -->
+        <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <!-- bootstrap cdn  -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+            integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <!-- fullcalendar css  -->
+        <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.8.0/main.css' rel='stylesheet' />
+    </head>
+        <!-- <div class="row mx-auto justify-content-center">
+        <div class="col-lg-8">
+                    <div id="calendar"></div>
+                </div>
+        </div> -->
+        <div class="row mx-auto justify-content-center">
+                            <div class="col-lg-8">
+                                <div class="card">
+                                    <div class="card-header">
+                                        Kalender
+                                    </div>
+                                    <div class="card-body">
+                                        <div id='calendar'></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.8.0/main.js'></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"
+            integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
+                var calendarEl = document.getElementById('calendar');
+                var today = new Date();
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                    //plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list' ],
+                    header: {
+                        left: 'prevYear,prev,next,nextYear today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+                    },
+                    initialView: 'dayGridMonth',
+                    defaultDate: today,
+                    navLinks: false,
+                    editable: false,
+                    eventLimit: false,
+                    events: [
+                        @foreach($info_agenda as $item)
+                      {
+                      title : '{{ $item->nama}}',
+                      start : '{{ $item->tanggal}}',
+                      time  : '{{ $item->jam}}'
+                    
+                      
+                    },
+
+                @endforeach
+                     ],
+                    selectOverlap: function (event) {
+                        return event.rendering === 'background';
+                    }
+                });
+    
+                calendar.render();
+            });
+        </script>
+        
     </section>
 @endsection
