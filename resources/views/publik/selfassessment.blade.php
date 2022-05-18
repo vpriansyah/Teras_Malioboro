869bdb3b2d4a4dafe473d7567c1cac5e6e90e25e
FathulNisaAini <92559540+FathulNisaAini@users.noreply.github.com>
2022-05-18 08:37:52 +0700

    Update selfassessment.blade.php

--- resources/views/publik/selfassessment.blade.php
+++ resources/views/publik/selfassessment.blade.php
@@ -8,36 +8,13 @@
           <div class="heading-title-layanan text-center">
             <h2 class="title iq-tw-6">Self Assessment</h2>
           </div>
-          <div id="reader" width="600px"></div>
         </div>
+        <center>
+        <div class="row justify-content-center">
+          <a href="/publik/scanner" type="button" class="btn btn-success btn-lg" > Silahkan Klik untuk melanjutkan scan</a>  
+        </center>  
+        </div> 
       </div>
-      <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
-
-      <script>
-                function onScanSuccess(decodedText, decodedResult) {
-          // handle the scanned code as you like, for example:
-          console.log(`Code matched = ${decodedText}`, decodedResult);
-        }
-
-        function onScanFailure(error) {
-          // handle scan failure, usually better to ignore and keep scanning.
-          // for example:
-          console.warn(`Code scan error = ${error}`);
-        }
-
-        let html5QrcodeScanner = new Html5QrcodeScanner(
-          "reader",
-          { fps: 10, qrbox: {width: 250, height: 250} },
-          /* verbose= */ false);
-        html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-      </script>
-    </div>
-    <div>
-      <p></p>
-      <center>
-      <p>Klik Allow agar bisa izinkan akses kamera untuk scan barcode Teras Malioboro</p>
-      <p> Setelah itu arahkan kamera pada barcode Teras Malioboro yang ingin di scan</p>
-        </center>
     </div>
 </section>
 @endsection
