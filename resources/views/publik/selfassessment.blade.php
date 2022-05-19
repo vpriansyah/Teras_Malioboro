94407bc1603ab239d86ef8702a1db54a6c537e04
FathulNisaAini <92559540+FathulNisaAini@users.noreply.github.com>
2022-05-19 14:08:46 +0700

    Update selfassessment.blade.php

--- resources/views/publik/selfassessment.blade.php
+++ resources/views/publik/selfassessment.blade.php
@@ -9,11 +9,21 @@
             <h2 class="title iq-tw-6">Self Assessment</h2>
           </div>
         </div>
+        
+        </div>
         <center>
-        <div class="row justify-content-center">
-          <a href="/publik/scanner" type="button" class="btn btn-success btn-lg" > Silahkan Klik untuk melanjutkan scan</a>  
-        </center>  
-        </div> 
+        <div class="card" style="width: 500px;">
+       <div class="card-body text-center">
+        <h5 class="card-title">Persyaratan</h5>
+        <p class="card-text">
+        <p>~ Wisatawan wajib sudah divaksinasi Covid-19, minimal dosis kedua</p>
+        <p>~ Berusia di atas 12 tahun</p>
+        <p>~ Menerapkan peraturan protokol kesehatan Cleanliness, Health, Safety, Environmental Sustainability (CHSE)</p>
+        </p>
+        <a href="/publik/scanner" type="button" class="btn btn-success btn-lg" > Setuju</a>  
+        </center>
+    </div>
+</div>
       </div>
     </div>
 </section>
