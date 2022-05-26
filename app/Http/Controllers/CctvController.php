19b6ea276b5431f3f0bed43ec332f2075c14991d
MuadzVarokah <muadzyo@gmail.com>
2022-05-26 20:15:29 +0700

    Update CctvController.php

--- app/Http/Controllers/CctvController.php
+++ app/Http/Controllers/CctvController.php
@@ -11,8 +11,19 @@ class CctvController extends Controller
 {
     public function index()
     {
-        $cctv = DB::table('cctv')->where('id', 4)->get();
-        return view('publik.cctv', ['cctv' => $cctv]);
-    }
+        $tm1 = DB::table('cctv')->where('lokasi', 1)->latest('id')->first();
+        $tm2 = DB::table('cctv')->where('lokasi', 2)->latest('id')->first();
+        $stat_tm1 = DB::table('stat_cctv')->where('id', $tm1->status)->first();
+        $stat_tm2 = DB::table('stat_cctv')->where('id', $tm2->status)->first();
 
+        // $data = ['tm1' => $tm1, 'tm2' => $tm2, 'stat_tm1' => $stat_tm1, 'stat_tm2' => $stat_tm2];
+        // dd($data);
+        
+        return view('publik.cctv', [
+            'tm1' => $tm1,
+            'tm2' => $tm2,
+            'stat_tm1' => $stat_tm1,
+            'stat_tm2' => $stat_tm2
+        ]);
+    }
 }
