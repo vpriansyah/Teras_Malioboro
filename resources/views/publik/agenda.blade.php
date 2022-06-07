d4a46930746566a58a05fa331601e3d32a6fe953
Dicky <dickylumbantobing@student.uns.ac.id>
2022-06-07 13:25:26 +0700

    Update agenda.blade.php
    up lagi agenda

--- resources/views/publik/agenda.blade.php
+++ resources/views/publik/agenda.blade.php
@@ -92,11 +92,11 @@
 
     {{-- Modal Informasi Agenda --}}
     <div class="modal fade" id="infoo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-        <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-dialog modal-dialog-scrollable modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
+                <h4 class="modal-title iq-tw-6" id="modalMdTitle">Agenda Teras Malioboro</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="modalMdTitle"></h4>
                 </div>
                 <div class="modal-body">
                     <div class="modalError"></div>
