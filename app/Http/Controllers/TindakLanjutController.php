e46b94e3e5e7a9f4fde084e8a3de8a2486ed936a
hemasworo <89896558+hemasworo@users.noreply.github.com>
2022-06-11 07:20:10 +0700

    update tindak lanjut

--- app/Http/Controllers/TindakLanjutController.php
+++ app/Http/Controllers/TindakLanjutController.php
@@ -6,22 +6,17 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class TindakLanjut extends Controller
+class TindakLanjutController extends Controller
 {    
     public function index(Request $request)
     {
-        $tindak_lanjut = TindakLanjut::table('tindak_lanjut')->paginate(2);
+        $tindak_lanjut = DB::table('tindak_lanjut')->paginate(2);
         return view('tindak.lanjut', ['tindak_lanjut' => $tindak_lanjut]);
     }
 
     public function search(Request $request)
     {
-        if($request->has('search')) {
-            $search = TindakLanjut::where('judul', 'like', "%" .$request->search. "%")->get();
-        }
-        else {
-            $search = TindakLanjut::all();
-        }
-        return view('tindak.lanjut', ['TindakLanjut' => $search]);
+        $search = DB::table('tindak_lanjut')->where('judul', 'like', "%" . $request->search . "%")->paginate(2);
+        return view('tindak.lanjut', ['tindak_lanjut' => $search]);
     }
 }
