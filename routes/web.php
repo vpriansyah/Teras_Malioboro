294aa2128be99eb6307bdbb2cdd32266b8666c62
arjunsetyawan <harjunosetyawan@gmail.com>
2022-06-14 17:33:00 +0700

    ganti role

--- routes/web.php
+++ routes/web.php
@@ -66,10 +66,6 @@ Route::get('/readmore-info/{info}', 'App\Http\Controllers\InfoPedagangController
 
 Route::get('/tindaklanjut/search', [TindakLanjutController::class, 'search']);
 
-Route::group(['middleware' => ['cekrole:admin,eksekutif']], function(){
-    Route::get('/pedagang', function () {
-        return view('/pedagang/dashboard');});
-});
 
 //Publik
 Route::get('/', [PublikController::class, 'index']);
